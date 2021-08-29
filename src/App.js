3b115ad99786943cfd5eef817cfcd885de94f0fc
Javahzade <cavid.haxizade@gmail.com>
2021-08-29 17:19:46 +0400

    fix

--- src/App.js
+++ src/App.js
@@ -1,25 +1,70 @@
-import logo from './logo.svg';
+import { Component, Fragment } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+export default class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      counter: 0,
+    }
+    console.log('constructor props =>', props);
+  }
 
-export default App;
+  static getDerivedStateFromProps(props, state){
+    console.log('getDerivedStateFromProps props =>', props);
+    console.log('getDerivedStateFromProps state =>', state);
+    return null;
+  }
+
+  getSnapshotBeforeUpdate(prevProps, prevState){
+    console.log('getSnapshotBeforeUpdate prevProps =>', prevProps);
+    console.log('getSnapshotBeforeUpdate prevState =>', prevState);
+    return null;
+  }
+
+  componentDidUpdate(prevProps, prevState, snapshot){
+    console.log('componentDidUpdate prevProps =>', prevProps);
+    console.log('componentDidUpdate prevState =>', prevState);
+    console.log('componentDidUpdate snapshot =>', snapshot);
+  }
+  componentDidMount(){
+    console.log('componentDidMount');
+  }
+  shouldComponentUpdate(nextProps, nextState){
+    console.log('shouldComponentUpdate nextProps =>', nextProps);
+    console.log('shouldComponentUpdate nextState =>', nextState);
+    return true;
+  }
+
+  increment = () => {
+    this.setState({
+      counter: this.state.counter + 1,
+    })
+  }
+  decrement = () => {
+    this.setState({
+      counter: this.state.counter - 1,
+    })
+  }
+  onReset = () => {
+    this.setState({
+      counter: 0,
+    })
+  }
+
+  render(){
+    console.log('render')
+    return(
+      <div style={{display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', }}>
+        <h1>Example</h1>
+        <h2>Counter: {this.state.counter}</h2>
+        {this.state.counter === 5 ? <h2>Max counter must be 5!</h2> : <Fragment />}
+        <div style={{display: 'flex'}}>
+          <button disabled={this.state.counter === 5} onClick={this.increment}>Increment</button>
+          <button onClick={this.decrement}>Decrement</button>
+          <button onClick={this.onReset}>Reset</button>
+        </div>
+      </div>
+    )
+  }
+}
